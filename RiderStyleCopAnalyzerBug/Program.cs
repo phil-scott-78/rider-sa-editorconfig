c1b6f87541067489413613608967852c0b204e38
Phil Scott <phil.scott@gmail.com>
2021-06-02 21:31:04 -0400

    bug repo

---
+++ RiderStyleCopAnalyzerBug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RiderStyleCopAnalyzerBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
